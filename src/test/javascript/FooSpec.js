88091f207ea7d5bca1ac35847b95ec4fa40d7df8
Kyle Krull <kyle.krull@here.com>
2014-11-12 17:06:23 -0600

    Trying to use underscore

--- src/test/javascript/FooSpec.js
+++ src/test/javascript/FooSpec.js
@@ -2,4 +2,7 @@ describe('Foo', function() {
   it('exists', function() {
     expect(Foo).toBeDefined();
   });
+  it('can use external libraries', function() {
+    expect(_).toBeDefined(); //TODO KDK: Work here to include webjars source
+  });
 });
