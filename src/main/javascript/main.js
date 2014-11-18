ca860c598fa699fae46b8041a6b6a9ead28c30f0
Kyle Krull <kyle.krull@here.com>
2014-11-17 18:39:11 -0600

    Appending templates to main body works for production, but there's still a difference in context root used for src/main/webapp between Jasmine and Jetty

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -1,5 +1,11 @@
 $(function main() {
-  var model = new Person({firstName: 'George'});
-  var view = new GreeterView({model: model});
-  view.render();
+  //TODO KDK: This path exists as /webapp/greeting/templates.html under Jasmine
+  $.get('greeting/templates.html', function(content) {
+    console.log('Loading templates');
+
+    $('body').append(content);
+    var model = new Person({firstName: 'George'});
+    var view = new GreeterView({model: model});
+    view.render();
+  });
 });
