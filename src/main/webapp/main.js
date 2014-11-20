5ffda988b5742b97272ff3a68a67acffd19c88f5
Kyle Krull <kyle.krull@here.com>
2014-11-20 16:38:39 -0600

    Refactoring

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,17 +1,15 @@
-//Script loading and intitialization for the production environment.  Loads with other scripts; runs on document ready.
+//Script loading and initialization for the production environment.  Loads with other scripts; runs on document ready.
 (function main() {
-  function renderView() {
-    var model = new Person({id: 42});
-    model.fetch({
-      success: function(updatedModel) {
-        var view = new GreeterView({model: updatedModel});
-        view.render();
-      }
-    });
+  function renderView(updatedModel, _response, _options) {
+    var view = new GreeterView({model: updatedModel});
+    view.render();
   }
 
   $(function() {
     console.debug('Setting up production environment from main.js');
-    sandbox.environment.loadTemplate('greeting/templates.html', renderView);
+    sandbox.environment.loadTemplate('greeting/templates.html', function() {
+      var model = new Person({id: 42});
+      model.fetch({success: renderView});
+    });
   });
 })();
